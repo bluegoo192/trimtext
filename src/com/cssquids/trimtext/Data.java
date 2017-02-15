b57a6fa3dc87ee39ca5f7599954ef1858883b55b
bluegoo192 <nikazuf4@gmail.com>
2017-02-15 00:00:26 -0800

    added stub data class

---
+++ src/com/cssquids/trimtext/Data.java
@@ -0,0 +1,11 @@
+package com.cssquids.trimtext;
+
+/**
+ * Created by Arthur on 2/14/2017.
+ */
+
+//The Data class is a singleton that provides central access to all data
+//In the future, it should provide Flux-like data management for easy access
+//Yes, this breaks the entire point of variable scope.  But it will save us so much time
+public class Data {
+}
