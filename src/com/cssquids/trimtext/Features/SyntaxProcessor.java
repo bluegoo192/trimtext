a8067d9f3bc914e3d369c095df3c9b92e2f573ab
bluegoo192 <nikazuf4@gmail.com>
2017-03-01 10:50:18 -0800

    added SyntaxProcessor class with stub methods

---
+++ src/com/cssquids/trimtext/Features/SyntaxProcessor.java
@@ -0,0 +1,29 @@
+package com.cssquids.trimtext.Features;
+
+import com.cssquids.trimtext.Languages.Language;
+import javafx.scene.text.Text;
+
+/**
+ * Created by Arthur on 2/17/2017.
+ * Should be a generic, language-independent(or dependent) syntax blob
+ * Like for Java, SyntaxProcessor objects would include stuff like DataType, Scope, FieldName...
+ * (You can see how syntax objects can be reused between languages)
+ */
+public class SyntaxProcessor {
+
+    private Language language;
+    private String text;
+
+    public SyntaxProcessor(Language lang) {
+        language = lang;
+    }
+
+    public Language process(String s) {//return the language of the string
+        this.text = s;
+        return new Language();//stub
+    }
+
+    public Text getNextSnippet() {
+        return new Text(text);
+    }
+}
---
+++ src/com/cssquids/trimtext/Languages/Language.java
@@ -0,0 +1,7 @@
+package com.cssquids.trimtext.Languages;
+
+/**
+ * Created by Arthur on 3/1/2017.
+ */
+public class Language {
+}
